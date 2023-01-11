058124f54758838d720992c12543331cc45f3cfe
Aliaksandr Yablonski <sasha432301@gmail.com>
2023-01-11 15:15:00 +0300

    idk

--- 1/index.js
+++ 1/index.js
@@ -6,11 +6,11 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 
 const fileName = "test.json";
+const usersFile = "users.json";
 
 const fileExist = function (req, res, next) {
   let method = req.method;
   let flag = fs.existsSync("./test.json");
-
   if (method === "POST") {
     return flag
       ? res.status(500).json({ message: "File already created" })
@@ -21,10 +21,27 @@ const fileExist = function (req, res, next) {
     : res.status(500).json({ message: "File is not found!" });
 };
 
-app.use(fileExist);
+const userExist = function (req, res, next) {
+  let user = req.query;
+  let fileContent = fs.readFileSync(usersFile, "utf8");
+  let usersArr = JSON.parse(fileContent).users;
+  let flag = false;
+  usersArr.forEach((el) => {
+    if (el.username === user.username && el.passwords === user.passwords) flag = true;
+  });
+  return flag
+    ? next()
+    : res.status(500).json({ message: "User is not found!" });
+};
+
+const middlewareCompose = function(arrFunc){
+
+}
+// app.use(userExist);
+// app.use(fileExist);
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", fileExist,(req, res) => {
   let options = {
     root: path.join(__dirname),
   };
@@ -38,11 +55,11 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/", (req, res) => {
+app.post("/",[userExist,fileExist], (req, res) => {
   res.send(fs.writeFileSync("test.json", JSON.stringify(req.body)));
 });
 
-app.put("/", function (req, res) {
+app.put("/",[userExist,fileExist] ,function (req, res) {
   let obj = req.body;
   let fileContent = fs.readFileSync(fileName, "utf8");
   let fileObj = JSON.parse(fileContent);
@@ -52,7 +69,7 @@ app.put("/", function (req, res) {
   res.send(fs.writeFileSync(fileName, JSON.stringify(fileObj)));
 });
 
-app.delete("/", function (req, res) {
+app.delete("/", [userExist,fileExist],function (req, res) {
   fs.unlink(fileName, (err) => {
     if (err) {
       res.status(500).json({ message: "File is not found" });
@@ -60,6 +77,14 @@ app.delete("/", function (req, res) {
       res.status(201).json({ message: "File deleted!" });
     }
   });
+});
+
+app.post("/users", (req, res) => {
+  let obj = req.query;
+  let fileContent = fs.readFileSync(usersFile, "utf8");
+  let usersObj = JSON.parse(fileContent);
+  usersObj["users"].push(obj);
+  res.send(fs.writeFileSync(usersFile, JSON.stringify(usersObj)));
 });
 
 app.listen(port, () => {
